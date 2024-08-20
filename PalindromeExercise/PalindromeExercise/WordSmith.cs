dfe4ffee67e792d31fa78c203c40eb2dff872203
Lydia Zamora <lydiazamorayoga@gmail.com>
2024-08-20 13:16:35 -0700

    Palindrome Tested

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+namespace PalindromeExercise
+{
+	public class WordSmith
+	{
+      
+       public bool IsAPalindrome(string word)
+        {
+            var reverse = "";
+
+            for( int i = word.Length -1; i >= 0; i--)
+            {
+                reverse += word[i]; 
+            }
+
+            if(word == reverse)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+           
+        }
+       
+
+
+    }
+}
+
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,21 @@
 using System;
+using PalindromeExercise;
 using Xunit;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("chowder", false)]
+        public void PalindromeTest(string word, bool expected)
         {
+            var palindrome = new WordSmith();
 
+            var actual = palindrome.IsAPalindrome(word);
+
+            Assert.Equal(expected, actual);
         }
     }
 }
